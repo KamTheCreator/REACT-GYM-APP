12ba0d134df7a1355d2d04032b85ad6330fddd4d
KamTheCreator <kammykamsi@gmail.com>
2024-10-01 22:26:38 +0100

    recent

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React  from 'react'
 import SectionWrapper from './SectionWrapper'
-import { WORKOUTS } from '../utils/swoldier'
+import { SCHEMES, WORKOUTS } from '../utils/swoldier'
+import { useState } from 'react'
 
 function Header(props){
   const {index, title,description } = props
@@ -18,23 +19,74 @@ function Header(props){
 }
 
 export default function Generator() {
+  const [showModal, setShowModal] = useState(false)
+  const [poison, setPoison] = useState('individual')
+  const [muscles, seMuscles] = useState([])
+  const [goal, setGoal] = useState('strength_power')
+
+
+  function toggleModal(){
+    setShowModal (!showModal)
+  }
+   
+
   return (
-    <SectionWrapper header={"generate your workout "} 
+    <SectionWrapper header={"generate your workout "}  
     title={['it\'s' , 'huge', '0\'clock' ]}>
+
       <Header index={'01'} title={'pick your poison'} 
       description={'select the workout your wish to do '}/>
       <div className='grid gird-cols-2 sm:grid-cols-4 gap-4'>
 
      {Object.keys(WORKOUTS).map ((type, typeIndex) =>{
        return(
-         <button key={typeIndex}>
-          <p>{type}</p>
+         <button onClick={() =>{
+          setPoison(type)
+
+         }} 
+         className={'bg-slate-950  border border-blue-400  duration-200 hover:border-blue-600py-3 rounded-lg' +
+           (type === poison ? ' ' : ' border-blue-400'  )} 
+         key={typeIndex}>
+          <p className='capitalize '> {type.replace('_',"")}</p>
         </button>
       )
       
     })}
     </div>
-    
+    <Header index={'02'} title={'lock on traget'} 
+      description={'select the muscles workout you want '}/>
+      <div className='bg-slate-950  border-solid border-blue-400  flex rounded-lg   flex-col'>
+        <button onClick={toggleModal} 
+        className=' relative py-3 flex items-center justify-center'>
+           <p> Select muscle groups</p>
+           <i className="fa-solid absolute right-3 top-1/2 -translate-y-1/2  fa-caret-down"></i>
+        </button>
+        {showModal &&  (
+           <div className='flex flex-col '> modal</div>
+           )}
+
+    </div>
+    <Header index={'03'} title={'become juggeranut'} 
+      description={'select your ultimate objective '}/>
+      {/* <div className='grid grid-cols-3  sm:grid-cols-4 gap-4'> */}
+      <div className='grid grid-cols-1 sm:grid-cols-3 gap-4'>
+
+     {Object.keys(SCHEMES).map ((scheme, schemeIndex) => {
+       return(
+        <button onClick={() =>{
+         setGoal(scheme)
+
+        }} 
+        className={'bg-slate-950  border border-blue-400  duration-200 hover:border-blue-600py-3 rounded-lg' +
+          (scheme === goal ? ' ' : ' border-blue-400'  )} 
+        key={schemeIndex}>
+         <p className='capitalize '> {scheme.replace('_',"")}</p>
+       </button>
+     )
+     
+      
+    })}
+    </div>
      
     </SectionWrapper>
   )
--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 export default function SectionWrapper(props) {
     const {children,header,title} = props
-    
+
   return (
-    <section className='min-h-screen flex flex-col gap-10'>
+    <section className='min-h-screen flex flex-col gap-10 '>
       <div className='bg-slate-950 py-10 flex flex-col gap-2 
-      justify-center items-center '>
+      justify-center items-center  p-4'>
         <p className='uppercase font-medium'>
           {header}
         </p>
@@ -20,7 +20,7 @@ export default function SectionWrapper(props) {
             </span>
         </h2>
       </div>
-      <div className='max-w-[800px] w-full flex flex-col mx-auto gap-10 '>
+      <div className='max-w-[800px] w-full flex flex-col mx-auto gap-10  p-4'>
 
       </div>
       {children}
